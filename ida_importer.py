c8ff7f7beda325d4209fcbb5d7333cd1cc6ac183
sporst <sp@porst.tv>
2010-04-26 15:57:51 +0200

    - Added license and readme - Added support for imported functions

--- ida_importer.py
+++ ida_importer.py
@@ -1,55 +1,103 @@
+# zynamics msdn-ida (http://github.com/zynamics/msdn-plugin-ida)
+# Copyright (C) 2010
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 from xml_parser import parse, FunctionHandler
 from textwrap import *
 
 wrapper = TextWrapper(break_long_words=False, width=70)
 
-functions = parse("C:\\Programme\\IDA56\\bincrowd-plugin-ida\\msdn.xml")
+functions = parse("C:\\code\\tools\\msdn-crawler\\msdn.xml")
 
 library = idc.GetInputFile().lower()
 
-library_functions = sorted([function for function in functions if function.dll.lower() == library])
-
 functions_map = { }
 
-for lf in library_functions:
-	functions_map[lf.name] = lf
+for lf in functions:
+    functions_map[lf.name] = lf
 
 assigned = 0
 not_assigned = 0
-	
+    
 for ea in Functions(0, 0xFFFFFFFF):
-	function = idaapi.get_func(ea)
-	if not function:
-		continue
-	
-	name = Demangle(idc.GetFunctionName(ea), idc.GetLongPrm(INF_SHORT_DN))
-	
-	if not name:
-		continue
-		
-	first_parens = name.find("(")
-	if first_parens != -1:
-		name = name[0:first_parens]
+    function_object = idaapi.get_func(ea)
+    if not function_object:
+        continue
+    
+    name = Demangle(idc.GetFunctionName(ea), idc.GetLongPrm(INF_SHORT_DN))
+    
+    if not name:
+        continue
+        
+    first_parens = name.find("(")
+    if first_parens != -1:
+        name = name[0:first_parens]
 
-	if functions_map.has_key(name):
-		description = functions_map[name].description.encode("UTF-8")
-		idaapi.set_func_cmt(function, "\n".join(wrapper.wrap(description)), True)
-		print "Assigned to %s" % name
-		assigned = assigned + 1
-	elif functions_map.has_key(name[0:-1]): # hack for 'we have document for Foo' but function is FooA/FooW
-		description = functions_map[name[0:-1]].description.encode("UTF-8")
-		idaapi.set_func_cmt(function, "\n".join(wrapper.wrap(description)), True)
-		print "Assigned to %s" % name
-		assigned = assigned + 1
-	else:
-		print "Not assigned to %s" % name
-		not_assigned = not_assigned + 1
-		
-print "Total %d" % len(library_functions)
+    if functions_map.has_key(name):
+        function = functions_map[name];
+        print "comparing %s to %s" % (function.dll.lower(), library)
+        if function.dll.lower() == library:
+            description = function.description.encode("UTF-8")
+            idaapi.set_func_cmt(function_object, "\n".join(wrapper.wrap(description)), True)
+            print "Assigned to %s" % name
+            assigned = assigned + 1
+    elif functions_map.has_key(name[0:-1]): # hack for 'we have document for Foo' but function is FooA/FooW
+        function = functions_map[name[0:-1]]
+        if function.dll.lower() == library:
+            description = function.description.encode("UTF-8")
+            idaapi.set_func_cmt(function_object, "\n".join(wrapper.wrap(description)), True)
+            print "Assigned to %s" % name
+            assigned = assigned + 1
+    else:
+        print "Not assigned to %s" % name
+        not_assigned = not_assigned + 1
+        
+current_dll = ""
+
+def imported_functions_callback(ea, name, ord):
+    """ Callback function for enumerating all imported functions of a module.
+    """
+    
+    global assigned
+    global not_assigned
+    
+    if functions_map.has_key(name):
+        function = functions_map[name];
+        if function.dll.lower() == current_dll:
+            description = function.description.encode("UTF-8")
+            idaapi.set_cmt(ea, "\n".join(wrapper.wrap(description)), True)
+            print "Assigned to %s" % name
+            assigned = assigned + 1
+    elif functions_map.has_key(name[0:-1]): # hack for 'we have document for Foo' but function is FooA/FooW
+        function = functions_map[name[0:-1]];
+        if function.dll.lower() == current_dll:
+            description = function.description.encode("UTF-8")
+            idaapi.set_cmt(ea, "\n".join(wrapper.wrap(description)), True)
+            print "Assigned to %s" % name
+            assigned = assigned + 1
+    else:
+        print "Not assigned to %s" % name
+        not_assigned = not_assigned + 1
+
+    return 1
+
+for import_index in xrange(idaapi.get_import_module_qty()):
+    current_dll = idaapi.get_import_module_name(import_index).lower() + ".dll"
+    idaapi.enum_import_names(import_index, imported_functions_callback)
+
 print "Assigned %d" % assigned
 print "Not assigned %d" % not_assigned
-		
 
-#dll_names = sets.Set([function.dll for function in functions])
-#for dll in sorted(dll_names):
-#	print dll
---
+++ readme.txt
@@ -0,0 +1,19 @@
+zynamics ida-msdn - Copyright 2010
+For updates please check http://github.com/zynamics/msdn-plugin-ida
+
+Using the zynamics ida-msdn IDA Pro plugin you can import Windows API
+documentation from the MSDN into your IDA Pro IDB files.
+
+1. Usage
+
+- At first you have to extract MSDN documentation from a local installation
+  of the MSDN. You can use the zynamics MSDN crawler for this which you can
+  download from http://github.com/zynamics/msdn-crawler
+- Once the MSDN crawler has generated the msdn.xml file you have to adjust
+  the path to the msdn.xml file in ida_importer.py
+- Using IDAPython 1.3.2 or higher you can then execute ida_importer.py
+
+2. License
+
+The MSDN crawler is licensed under the GPL 2.0 license. Please see gpl.txt for
+more information.
